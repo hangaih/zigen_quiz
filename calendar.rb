dff811467ba47b6bfbf9ca933484db0b6503458b
hangaih <hangai.acc@gmail.com>
2020-02-24 10:37:48 +0900

    2次元配列でカレンダーを表現

---
+++ calendar.rb
@@ -0,0 +1,42 @@
+day_of_week = ['日','月', '火', '水', '木', '金', '土']
+day_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_month_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+puts day_of_month.sum()
+
+
+total_day = 1
+ary = []
+day = 0
+month = 1
+year = 1
+for num in total_day..800000 do
+  if year % 4 == 0
+    if month == 13
+      month = 1
+      year += 1
+    end
+    if day >= day_of_month_leap[month-1]
+      day = 1
+      month += 1
+    else
+      day += 1
+    end
+  else
+    if month == 13
+      month = 1
+      year += 1
+    end
+    if day >= day_of_month[month-1]
+      day = 1
+      month += 1
+    else
+      day += 1
+    end
+  end
+
+  ary.push([year, month, day, day_of_week[total_day % 7], total_day])
+  total_day += 1
+end
+p ary
+
+
