9e69f5ad38946bd55d1a222fb0c755960b07b845
hangaih <hangai.acc@gmail.com>
2020-02-24 11:55:13 +0900

    閏年の規則の追加

--- calendar.rb
+++ calendar.rb
@@ -3,25 +3,39 @@ day_of_week = ['日','月', '火', '水', '木', '金', '土']
 day_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_of_month_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def new_year(month, year)
-  if month == 13
-    month = 1
-    year += 1
-  end
-end
-
 total_day = 1
 ary = []
 day = 0
 month = 1
 year = 1
 for num in total_day..737500 do
-  if year % 4 == 0
-#    if month == 13
-#      month = 1
-#      year += 1
-#    end
-    new_year(month, year)
+  if year % 400 == 0
+    if month == 13
+      month = 1
+      year += 1
+    end
+    if day >= day_of_month_leap[month-1]
+      day = 1
+      month += 1
+    else
+      day += 1
+    end
+  elsif year % 100 == 0
+    if month == 13
+      month = 1
+      year += 1
+    end
+    if day >= day_of_month[month-1]
+      day = 1
+      month += 1
+    else
+      day += 1
+    end
+  elsif year % 4 == 0
+    if month == 13
+      month = 1
+      year += 1
+    end
     if day >= day_of_month_leap[month-1]
       day = 1
       month += 1
@@ -29,11 +43,10 @@ for num in total_day..737500 do
       day += 1
     end
   else
-#    if month == 13
-#    month = 1
-#    year += 1
-#    end
-    new_year(month, year)
+    if month == 13
+    month = 1
+    year += 1
+    end
     if day >= day_of_month[month-1]
       day = 1
       month += 1
